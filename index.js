3d6d83575217d2951c24ca5d7dee4af5f7a639a8
Caroline Lopes <caroline.lopesm@hotmail.com>
2020-04-20 16:18:08 -0300

    uma animação

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* --------------------------------- form ---------------------------------*/
 function getFormData (e){
     e.preventDefault()  
     const name = document.getElementById('name').value;
@@ -8,3 +9,26 @@ function getFormData (e){
      + ' " ' + msg +  ' ", '  + " foi enviada com sucesso!"
 return response
 }
+
+
+
+/* --------------------------------- animação scroll ---------------------------------*/
+
+const target = document.querySelectorAll('[data-animate]');
+const animationClass = 'animate'
+
+function animeScroll(){
+const windowTop = window.pageYOffset +  (window.innerHeight * 3) / 4 ;
+target.forEach(function(element){
+   if((windowTop) > element.offsetTop){
+       element.classList.add(animationClass)
+       console.log(element.offsetTop)
+   } else{
+       element.classList.remove(animationClass)
+   }
+})
+}
+window.addEventListener('scroll', function(){
+    animeScroll();
+}
+)
